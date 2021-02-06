166ea9ee4431aad15e478ca6d07e6da2a748db6c
Thiago Fonseca <thiagonfonseca@gmail.com>
2021-02-06 20:38:37 -0300

    Entrega de Projeto DIO

---
+++ src/main/java/dio/project/heroesapi/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package dio.project.heroesapi.repository;
+
+import dio.project.heroesapi.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String> {
+}
